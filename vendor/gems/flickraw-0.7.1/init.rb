bd09289b70e48d5a9585be544dc15be930aa3c0c
Ben <ben@pez.(none)>
2009-10-07 22:12:15 -0700

    updated default route

--- vendor/gems/flickraw-0.7.1/init.rb
+++ vendor/gems/flickraw-0.7.1/init.rb
@@ -1,9 +1,10 @@
 
-require_options = ["flickraw"]
-if require_lib = require_options.find { |path|  File.directory?(File.join(File.dirname(__FILE__), 'lib', path)) }
-  require File.join(File.dirname(__FILE__), 'lib', require_lib)
-else
-  puts msg = "ERROR: Please update #{File.expand_path __FILE__} with the require path for linked RubyGem flickraw"
-  exit
-end
+require "#{File.dirname(__FILE__)}/lib/flickraw.rb"
+#require_options = ["flickraw"]
+#if require_lib = require_options.find { |path|  File.directory?(File.join(File.dirname(__FILE__), 'lib', path)) }
+#  require File.join(File.dirname(__FILE__), 'lib', require_lib)
+#else
+#  puts msg = "ERROR: Please update #{File.expand_path __FILE__} with the require path for linked RubyGem flickraw"
+#  exit
+#end
 
