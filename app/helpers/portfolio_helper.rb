7bcaa907375adbe4973271d07d91098d2c972c31
Ben <ben@pez.(none)>
2009-10-13 22:45:25 -0700

    created photo viewer

--- app/helpers/portfolio_helper.rb
+++ app/helpers/portfolio_helper.rb
@@ -1,9 +1,17 @@
 module PortfolioHelper
-  def photo_class(photo)
+  def photo_width(photo)
     if photo.width_l.to_i > photo.height_l.to_i
-      "fat_photo"
+      500
     else
-      "skinny_photo"
+      ((500.0/photo.height_l.to_f) * photo.width_l.to_i).to_i
+    end
+  end
+
+  def photo_height(photo)
+    if photo.width_l.to_i > photo.height_l.to_i
+      ((500.0/photo.width_l.to_f) * photo.height_l.to_i).to_i
+    else
+      500
     end
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,29 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+if(!VP) var VP = {};
+VP.selected_photo_index = 0;
+
+VP.previous_image = function() {
+  if(VP.selected_photo_index == 1) {
+    $('previous_button').fade();
+  }
+  $('photo_'+VP.selected_photo_index).fade();
+  VP.selected_photo_index--;
+  $('photo_'+VP.selected_photo_index).appear();
+
+  if(!$('photo_'+(VP.selected_photo_index+2))) {
+    $('next_button').appear();
+  }
+};
+
+VP.next_image = function() {
+  if(VP.selected_photo_index == 0) {
+    $('previous_button').appear();
+  }
+  $('photo_'+VP.selected_photo_index).fade();
+  VP.selected_photo_index++;
+  $('photo_'+VP.selected_photo_index).appear();
+
+  if(!$('photo_'+(VP.selected_photo_index+1))) {
+    $('next_button').fade();
+  }
+};
+
