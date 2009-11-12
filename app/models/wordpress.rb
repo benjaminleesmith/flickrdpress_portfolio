ad6242a3dfed5f1f39642b3702f9df75f00c9c97
Ben <ben@pez.(none)>
2009-11-12 15:42:54 -0700

    consolidated config files, added google analytics

--- app/models/wordpress.rb
+++ app/models/wordpress.rb
@@ -1,6 +1,6 @@
 class Wordpress
   def self.posts
-    source = WORDPRESS_CONFIG['rss_url']
+    source = PORTFOLIO_CONFIG['wordpress_rss_url']
     content = ''
     open(source) do |s| content = s.read end
     REXML::Document.new(content).root.elements.to_a('channel/item')
