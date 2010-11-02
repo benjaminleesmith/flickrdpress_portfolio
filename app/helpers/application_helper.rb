c056bdb3e2bcf8d6f6f69eba66eb3096cdfce560
Benjamin Smith <benjamin.lee.smith@gmail.com>
2010-11-01 20:10:34 -0400

    added license

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
+# Copyright 2009, 2010 Benjamin Lee Smith <benjamin.lee.smith@gmail.com>
+#
+# This file is part of Voltron Portfolio.
+# Voltron Portfolio is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Voltron Portfolio is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Voltron Portfolio.  If not, see <http://www.gnu.org/licenses/>.
+
 module ApplicationHelper
   def title(page_title, show_title = true)
     @content_for_title = page_title.to_s
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,18 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// Copyright 2009, 2010 Benjamin Lee Smith <benjamin.lee.smith@gmail.com>
+//
+// This file is part of Voltron Portfolio.
+// Voltron Portfolio is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Voltron Portfolio is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Voltron Portfolio.  If not, see <http://www.gnu.org/licenses/>.
 
 if(!VP) var VP = {};
 VP.selected_photo_index = 0;
