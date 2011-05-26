f71379e0ee9aa3ed660febfb0622adca1b66f1f6
Benjamin Smith <benjamin.lee.smith@gmail.com>
2011-05-25 19:36:02 -0600

    finally got tests working

--- features/home.feature
+++ features/home.feature
@@ -4,15 +4,19 @@ Feature: Home
   Scenario: View Home Page/Blog/Contact
     Given I am on the home page
 	Then "NOTE: All my photos are copyrighted." should be visible
-	And "flickr gadget for google wave" should be hidden
-	And "If you would like to contact me about anything at all, feel free to email me at" should be hidden
+	And "flickr gadget for google wave" should be invisible
+	And "If you would like to contact me about anything at all, feel free to email me at" should be invisible
 	When I follow "Blog"
 	And I wait for the div with id "blog" to be visible
+	And I wait for the div with id "contact" to be invisible
+	And I wait for the div with id "home" to be invisible
 	Then "flickr gadget for google wave" should be visible
-	And "If you would like to contact me about anything at all, feel free to email me at" should be hidden
-	And "NOTE: All my photos are copyrighted." should be hidden
+	And "If you would like to contact me about anything at all, feel free to email me at" should be invisible
+	And "NOTE: All my photos are copyrighted." should be invisible
 	When I follow "Contact"
 	And I wait for the div with id "contact" to be visible
-	Then "NOTE: All my photos are copyrighted." should be hidden
-	And "flickr gadget for google wave" should be hidden
+	And I wait for the div with id "blog" to be invisible
+	And I wait for the div with id "home" to be invisible
+	Then "NOTE: All my photos are copyrighted." should be invisible
+	And "flickr gadget for google wave" should be invisible
 	And "If you would like to contact me about anything at all, feel free to email me at" should be visible
--- vendor/gems/common_cucumber_steps/lib/common_cucumber_steps.rb
+++ vendor/gems/common_cucumber_steps/lib/common_cucumber_steps.rb
@@ -1,36 +1,12 @@
 if defined? Cucumber 
-  # Then /^"([^\"]+)" should be hidden$/ do |text|
-  #   # TODO: doesn't work in text contains quotes
-  #   # TODO: doesn't work unless text is from the beginning of an html element
-  #   paths = [
-  #     "//*[@class='hidden']/*[contains(.,'#{text}')]",
-  #     "//*[@class='invisible']/*[contains(.,'#{text}')]",
-  #     "//*[@style='display: none;']/*[contains(.,'#{text}')]",
-  #     "//*[@style='display:none;']/*[contains(.,'#{text}')]"
-  #   ]
-  #   xpath = paths.join '|'
-  #   page.should have_xpath(xpath)
-  # end
-  
   # http://groups.google.com/group/cukes/browse_thread/thread/e02f94b17b4409b/a1cabf12b82081a5?pli=1
-  # Then /^(?:|I )should see "([^\"]*)"(?: within "([^\"]*)")?$/ do |text, selector|
-  #   with_scope(selector) do
-  #     if page.respond_to? :should
-  #       page.should have_xpath("//*[not(descendant-or-self::script) and not(contains(@style,'display: none;') and ancestor::*[not(contains(@style,'display: none;'))])][contains(normalize-space(.),#{Capybara::XPath.escape(text)})]")
-  #     else
-  #       assert page.has_xpath?("//*[not(descendant-or-self::script) and not(contains(@style,'display: none;') and ancestor::*[not(contains(@style,'display: none;'))])][contains(normalize-space(.),#{Capybara::XPath.escape(text)})]")
-  #     end
-  #   end
-  # end
-
-  # http://groups.google.com/group/cukes/browse_thread/thread/e02f94b17b4409b/a1cabf12b82081a5?pli=1
-  Then /^"([^\"]*)" should be hidden(?: within "([^\"]*)")?$/ do |text, selector|
+  Then /^"([^\"]*)" should be invisible(?: within "([^\"]*)")?$/ do |text, selector|
     # TODO: escape text
     with_scope(selector) do
       if page.respond_to? :should
-        page.should have_xpath("//*[not(descendant-or-self::script)][contains(@style,'display:none;') or ancestor::*[contains(@style,'display:none;')] and contains(@style,'display: none;') and ancestor::*[contains(@style,'display: none;')]][contains(normalize-space(.),'#{text}')]")
+        page.should have_xpath("//*[not(descendant-or-self::script)][contains(@style,'display:none;') or contains(@style,'display: none;') or ancestor::*[contains(@style,'display:none;') or contains(@style,'display: none;')]][contains(normalize-space(.),'#{text}')]")
       else
-        assert page.have_xpath?("//*[not(descendant-or-self::script)][contains(@style,'display:none;') and ancestor::*[contains(@style,'display:none;')] and contains(@style,'display: none;') and ancestor::*[contains(@style,'display: none;')]][contains(normalize-space(.),'#{text}')]")
+        assert have_xpath("//*[not(descendant-or-self::script)][contains(@style,'display:none;') or contains(@style,'display: none;') or ancestor::*[contains(@style,'display:none;') or contains(@style,'display: none;')]][contains(normalize-space(.),'#{text}')]")
       end
     end
   end
@@ -40,9 +16,9 @@ if defined? Cucumber
     # TODO: escape text
     with_scope(selector) do
       if page.respond_to? :should
-        page.should have_no_xpath("//*[not(descendant-or-self::script)][contains(@style,'display:none;') or ancestor::*[contains(@style,'display:none;')] and contains(@style,'display: none;') and ancestor::*[contains(@style,'display: none;')]][contains(normalize-space(.),'#{text}')]")
+        page.should have_no_xpath("//*[not(descendant-or-self::script)][contains(@style,'display:none;') or contains(@style,'display: none;') or ancestor::*[contains(@style,'display:none;') or contains(@style,'display: none;')]][contains(normalize-space(.),'#{text}')]")
       else
-        assert page.have_no_xpath?("//*[not(descendant-or-self::script)][contains(@style,'display:none;') and ancestor::*[contains(@style,'display:none;')] and contains(@style,'display: none;') and ancestor::*[contains(@style,'display: none;')]][contains(normalize-space(.),'#{text}')]")
+        assert page.have_no_xpath?("//*[not(descendant-or-self::script)][contains(@style,'display:none;') or contains(@style,'display: none;') or ancestor::*[contains(@style,'display:none;') or contains(@style,'display: none;')]][contains(normalize-space(.),'#{text}')]")
       end
     end
   end
@@ -51,8 +27,12 @@ if defined? Cucumber
     sleep(arg1.to_i)
   end
   
-  # TOOD Check to see if selenium is loaded
+  # TODO: Check to see if selenium is loaded?
   When /^I wait for the div with id "([^"]*)" to be visible$/ do |arg1|
-   wait_until { page.should have_xpath("//div[@id='#{arg1}' and contains(@style, '')]")}
+   wait_until { page.should have_xpath("//div[@id='#{arg1}' and not(contains(@style, 'display:none;' or contains(@style, 'display:none;')))]")}
+  end
+  
+  When /^I wait for the div with id "([^"]*)" to be invisible$/ do |arg1|
+   wait_until { page.should have_xpath("//div[@id='#{arg1}' and (contains(@style, 'display: none;') or contains(@style, 'display:none;'))]")}
   end
 end
